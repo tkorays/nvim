4e4f19bbaed3c7eda1ced356b829a4e9f76c3c67
tkorays <tkorays@hotmail.com>
2024-08-10 14:10:45 +0800

    modify commands

--- lua/plugins/commander.lua
+++ lua/plugins/commander.lua
@@ -4,241 +4,299 @@ return function()
     commander.setup {
         components = {
             'DESC',
-            'KEYS',
+            -- 'KEYS',
             'CMD',
             'CAT'
         },
         sort_by = {
             'DESC',
-            'KEYS',
+            -- 'KEYS',
             'CMD',
             'CAT'
         },
-        prompt_title = 'Commander'
+        prompt_title = 'QuicCmd',
+        separator = " ",
+        auto_replace_desc_with_cmd = true,
+        integration = {
+            telescope = {
+                enable = true
+            },
+            lazy = {
+                enable = false
+            }
+        }
     }
 
     commander.add({
         {
-            desc = 'cmake-build',
+            desc = '[CMake]build',
             cmd = '<CMD>CMakeBuild<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-configure',
+            desc = '[CMake]configure',
             cmd = '<CMD>CMakeGenerate<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-run',
+            desc = '[CMake]run',
             cmd = '<CMD>CMakeRun<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-run-test',
+            desc = '[CMake]run-test',
             cmd = '<CMD>CMakeRunTest<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-select-build-target',
+            desc = '[CMake]select-build-target',
             cmd = '<CMD>CMakeSelectBuildTarget<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-select-build-type',
+            desc = '[CMake]select-build-type',
             cmd = '<CMD>CMakeSelectBuildType<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-select-launch-target',
+            desc = '[CMake]select-launch-target',
             cmd = '<CMD>CMakeSelectLaunchTarget<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-settings',
+            desc = '[CMake]settings',
             cmd = '<CMD>CMakeSettings<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-clean',
+            desc = '[CMake]clean',
             cmd = '<CMD>CMakeClean<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-gtest',
+            desc = '[CMake]gtest',
             cmd = '<CMD>Telescope gtest<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-open-executor',
+            desc = '[CMake]open-executor',
             cmd = '<CMD>CMakeOpenExecutor<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-close-executor',
+            desc = '[CMake]close-executor',
             cmd = '<CMD>CMakeCloseExecutor<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-open-runner',
+            desc = '[CMake]open-runner',
             cmd = '<CMD>CMakeOpenRunner<CR>',
+            cat = 'CMake'
         },
         {
-            desc = 'cmake-close-runner',
+            desc = '[CMake]close-runner',
             cmd = '<CMD>CMakeCloseRunner<CR>',
+            cat = 'CMake'
         },
 
         {
-            desc = 'git-blame',
-            cmd = '<CMD>Git blame<CR>'
+            desc = '[Git]blame',
+            cmd = '<CMD>Git blame<CR>',
+            cat = 'Git'
         },
         {
-            desc = 'git-toggle-current-line-blame',
+            desc = '[Git]toggle-current-line-blame',
             cmd = '<CMD>Gitsigns toggle_current_line_blame<CR>',
+            cat = 'Git'
         },
         {
-            desc = 'git-commits',
-            cmd = '<CMD>Telescope git_commits<CR>'
-        },
-
-        {
-            desc = 'markdown-preview',
-            cmd = '<cmd>MarkdownPreview<cr>'
-        },
-        {
-            desc = 'markdown-preview-stop',
-            cmd = '<cmd>MarkdownPreviewStop<cr>'
-        },
-        {
-            desc = 'markdown-preview-toggle',
-            cmd = '<cmd>MarkdownPreviewToggle<cr>'
+            desc = '[Git]commits',
+            cmd = '<CMD>Telescope git_commits<CR>',
+            cat = 'Git'
         },
 
         {
-            desc = 'rename-symbol',
-            cmd = '<leader>sr',
-            keys = { 'n', '<leader>sr' }
+            desc = '[Markdown]preview',
+            cmd = '<cmd>MarkdownPreview<cr>',
+            cat = 'Markdown'
+        },
+        {
+            desc = '[Markdown]preview-stop',
+            cmd = '<cmd>MarkdownPreviewStop<cr>',
+            cat = 'Markdown'
+        },
+        {
+            desc = '[Markdown]preview-toggle',
+            cmd = '<cmd>MarkdownPreviewToggle<cr>',
+            cat = 'Markdown'
         },
 
         {
-            desc = 'show-buffers',
+            desc = '[LSP]rename-symbol',
+            cmd = function() vim.lsp.buf.rename() end,
+            cat = 'LSP'
+        },
+
+        {
+            desc = '[Telescope]show-buffers',
             cmd = '<leader>fb',
-            keys = { 'n', '<leader>fb' }
+            keys = { 'n', '<leader>fb' },
+            cat = 'Telescope'
         },
 
         {
-            desc = 'switch-colorscheme',
-            cmd = '<CMD>Telescope colorscheme<CR>'
+            desc = '[Telescope]change colorscheme',
+            cmd = '<CMD>Telescope colorscheme<CR>',
+            cat = 'Telescope'
         },
         {
-            desc = 'live-grep',
-            cmd = '<CMD>Telescope live_grep<CR>'
+            desc = '[Telescope]live-grep',
+            cmd = '<CMD>Telescope live_grep<CR>',
+            cat = 'Telescope'
         },
         {
-            desc = 'emoji',
-            cmd = '<CMD>Telescope emoji<CR>'
+            desc = '[Telescope]emoji',
+            cmd = '<CMD>Telescope emoji<CR>',
+            cat = 'Telescope'
         },
         {
-            desc = 'lazygit',
-            cmd = '<CMD>LazyGit<CR>'
+            desc = '[Git]lazygit',
+            cmd = '<CMD>LazyGit<CR>',
+            cat = 'Git'
         },
         {
-            desc = 'workspace-symbols',
+            desc = '[Telescope]workspace-symbols',
             cmd = '<leader>fS',
             keys = { 'n', '<leader>fS' },
+            cat = 'Telescope'
         },
         {
-            desc = 'document-symbols',
+            desc = '[Telescope]document-symbols',
             cmd = '<leader>fs',
             keys = { 'n', '<leader>fs' },
+            cat = 'Telescope'
         },
         {
-            desc = 'toggle-terminal',
+            desc = '[Terminal]toggle',
             cmd = '<CMD>ToggleTerm<CR>',
+            cat = 'Telescope'
         },
 
         {
-            desc = 'lsp-info',
+            desc = '[LSP]lsp-info',
             cmd = '<CMD>LspInfo<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lsp-start',
+            desc = '[LSP]start',
             cmd = '<CMD>LspStart<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lsp-stop',
+            desc = '[LSP]stop',
             cmd = '<CMD>LspStop<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lsp-restart',
+            desc = '[LSP]restart',
             cmd = '<CMD>LspRestart<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lsp-log',
+            desc = '[LSP]log',
             cmd = '<CMD>LspLog<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'code action',
-            cmd = function() vim.lsp.buf.code_action() end
+            desc = '[LSP]code action',
+            cmd = function() vim.lsp.buf.code_action() end,
+            cat = 'LSP'
         },
         {
-            desc = 'open-recent',
-            cmd = function() require('telescope').extensions.recent_files.pick() end
+            desc = '[Telescope]open-recent',
+            cmd = function() require('telescope').extensions.recent_files.pick() end,
+            cat = 'Telescope'
         },
         {
-            desc = 'neotest-open-summary',
+            desc = '[NeoTest]open-summary',
             cmd = function()
                 require('neotest').summary.open()
-            end
+            end,
+            cat = 'NeoTest'
         },
         {
-            desc = 'neotest-close-summary',
+            desc = '[NeoTest]close-summary',
             cmd = function()
                 require('neotest').summary.close()
-            end
+            end,
+            cat = 'NeoTest'
         },
         {
-            desc = 'neotest-toggle-summary',
+            desc = '[NeoTest]toggle-summary',
             cmd = function()
                 require('neotest').summary.toggle()
-            end
+            end,
+            cat = 'NeoTest'
         },
         {
-            desc = 'neotest-run-near',
+            desc = '[NeoTest]run-near',
             cmd = function()
                 require('neotest').run.run()
-            end
+            end,
+            cat = 'NeoTest'
         },
         {
-            desc = 'current filename',
+            desc = '[Core]current filename',
             cmd = "<CMD>f<CR>"
         },
         {
-            desc = 'trouble-diag-toggle',
-            cmd = '<CMD>Trouble diagnostics toggle<CR>'
+            desc = '[Trouble]diag-toggle',
+            cmd = '<CMD>Trouble diagnostics toggle<CR>',
+            cat = 'Trouble'
         },
         {
-            desc = 'overseear-toggle',
-            cmd = '<CMD>OverseerToggle<CR>'
+            desc = '[OverSeerTask]toggle',
+            cmd = '<CMD>OverseerToggle<CR>',
+            cat = 'Overseer'
         },
         {
-            desc = 'overseear-run',
-            cmd = '<CMD>OverseerRun<CR>'
+            desc = '[OverSeerTask]run',
+            cmd = '<CMD>OverseerRun<CR>',
+            cat = 'Overseer'
         },
         {
-            desc = 'lspsaga-outline',
-            cmd = '<CMD>Lspsaga outline<CR>'
+            desc = '[LSP]outline-toggle',
+            cmd = '<CMD>Lspsaga outline<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lspsaga-incoming-calls',
-            cmd = '<CMD>Lspsaga incoming_calls<CR>'
+            desc = '[LSP]incoming-calls',
+            cmd = '<CMD>Lspsaga incoming_calls<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lspsaga-outging-calls',
-            cmd = '<CMD>Lspsaga outgoing_calls<CR>'
+            desc = '[LSP]outging-calls',
+            cmd = '<CMD>Lspsaga outgoing_calls<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lspsage-code-action',
-            cmd = '<CMD>Lspsaga code_action<CR>'
+            desc = '[LSP]code-action-saga',
+            cmd = '<CMD>Lspsaga code_action<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lspsage-diag-file',
+            desc = '[LSP]diag-file-saga',
             cmd = '<CMD>Lspsaga show_buf_diagnostics<CR>'
         },
         {
-            desc = 'lspsage-diag-workspace',
-            cmd = '<CMD>Lspsaga show_workspace_diagnostics<CR>'
+            desc = '[LSP]diag-workspace-saga',
+            cmd = '<CMD>Lspsaga show_workspace_diagnostics<CR>',
+            cat = 'LSP'
         },
         {
-            desc = 'lspsaga-find-cursor',
-            cmd = '<CMD>Lspsaga finder<CR>'
+            desc = '[LSP]find-reference-saga',
+            cmd = '<CMD>Lspsaga finder<CR>',
+            cat = 'LSP'
         }
     }, {})
 
